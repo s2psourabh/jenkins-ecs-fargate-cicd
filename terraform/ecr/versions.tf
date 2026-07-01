cddfcacb50908607e06c0372e135b2f64a4e5107
s2psourabh <s2psourabh@gmail.com>
2026-07-01 17:59:15 +0530

    versions.tf and variables.tf files are added

---
+++ terraform/ecr/versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  backend "s3" {
+    key          = "jenkins-ecs-fargate-cicd/ecr/terraform.tfstate"
+    encrypt = true
+    use_lockfile = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type = string
+}
+
+variable "app_name" {
+    description = "Application Name"
+    type = string
+}
